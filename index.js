bf01b50408753fcc436fd721faf9c601516b1171
Amit Choukroun <amit@mtrn.co.il>
2017-04-18 11:43:31 +0300

    fix ready issue

--- index.js
+++ index.js
@@ -80,7 +80,6 @@ class MicroMice {
     this.config = _.defaults(config, { timeout: 2000 });
     this.ipc = new ipc.IPC();
     this.start = () => Promise.resolve();
-    this.ready = _.noop;
     this.ipc.config = _.defaults(config, {
       id: _.uniqueId('service'),
       retry: 1500,
@@ -102,7 +101,7 @@ class MicroMice {
     this._bindServices();
     this.ipc.server.start();
 
-    setTimeout(this.ready, 10);
+    setTimeout(() => this.ready(), 10);
   }
 
   _bindEvents() {
